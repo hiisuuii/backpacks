aff341c87eca37cfeeb608e954bd261a4389b390
hyperlynx <hjake123@gmail.com>
2025-08-13 21:05:25 -0500

    Prevent the backpack from being placed "inside itself", and thus deleting itself

---
+++ src/main/java/live/hisui/backpacks/menu/BackpackMenu.java
@@ -0,0 +1,35 @@
+package live.hisui.backpacks.menu;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.ItemStack;
+
+/*
+Extend the ChestMenu to be unable to shift-click the backpack into itself. Should fix the "backpack ouroboros" issue.
+ */
+public class BackpackMenu extends ChestMenu {
+    private final ItemStack self;
+
+    private BackpackMenu(MenuType<?> type, int containerId, Inventory playerInventory, int rows, Container container, ItemStack self) {
+        super(type, containerId, playerInventory, container, rows);
+        this.self = self;
+    }
+
+    public static BackpackMenu smallBackpack(int containerId, Inventory playerInventory, Container container, ItemStack self) {
+        return new BackpackMenu(MenuType.GENERIC_9x3, containerId, playerInventory, 3, container, self);
+    }
+
+    public static BackpackMenu largeBackpack(int containerId, Inventory playerInventory, Container container, ItemStack self) {
+        return new BackpackMenu(MenuType.GENERIC_9x6, containerId, playerInventory, 6, container, self);
+    }
+
+    @Override
+    protected boolean moveItemStackTo(ItemStack stack, int startIndex, int endIndex, boolean reverseDirection) {
+        if(ItemStack.isSameItemSameComponents(stack, self)) {
+            return false;
+        }
+        return super.moveItemStackTo(stack, startIndex, endIndex, reverseDirection);
+    }
+}
